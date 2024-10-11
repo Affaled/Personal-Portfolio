8b0a20b12203beb29f947fb9f638cd86d34599d9
Lucas Affaled <affaledl@gmail.com>
2024-10-10 22:29:40 -0300

    corrigindo bug de compatibilidade com postcss

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,15 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import postcss from "rollup-plugin-postcss";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    {
-      ...postcss({
-        extract: true,
-        sourceMap: true,
-        extensions: [".css", ".scss"],
-        minimize: false,
-        use: [
-          [
-            "sass",
-            {
-              includePaths: ["./src/styles"],
-            },
-          ],
-        ],
-      }),
-      enforce: "post",
+  plugins: [vue()],
+  css: {
+    devSourcemap: true,
+    preprocessorOptions: {
+      scss: {
+        includePaths: ["./src/styles"],
+      },
     },
-  ],
+  },
 });
